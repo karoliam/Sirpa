86696ae0401e27761bc5f2da0fb2b60685b426e8
Lor3wp <61760289+Lor3wp@users.noreply.github.com>
2022-12-02 18:28:47 +0200

    preworkout

---
+++ Sirpa/Sirpa/Models/MapMarkers.swift
@@ -0,0 +1,18 @@
+//
+//  MapMarkers.swift
+//  Sirpa
+//
+//  Created by iosdev on 2.12.2022.
+//
+
+import Foundation
+struct MapMarkers: Identifiable {
+    let id = UUID()
+    let lat: String
+    let long: String
+    let file: String
+    let notes: String
+    let timeStamp: String
+    let tripID: String
+    let userID: String
+}
