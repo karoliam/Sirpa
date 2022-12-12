42bc5f02e607ea36b4e4860b4bd5a1bedd37d45f
Konsta <konsta.perttila@hotmail.com>
2022-12-12 10:05:02 +0200

    Notes on pictures when tapped.

--- Sirpa/Sirpa/Views/PicturesInTripsView.swift
+++ Sirpa/Sirpa/Views/PicturesInTripsView.swift
@@ -7,46 +7,82 @@
 
 import SwiftUI
 
+
 struct PicturesInTripsView: View {
+    @State var notesShown = false
     var body: some View {
         NavigationView {
             ZStack{
-                VStack{
-                    ScrollView{
-                        VStack{
-                            /*List(imageDictionary.filter{
-                             $0.key.contains(item.id)
-                         }.map {
-                             $0.value
-                         }
-                              , id: .self) { item in
-
-                             Image(uiImage: item)
-                                 .resizable()
-                                 .frame(width: 200, height: 200)
-
-                         }
-                              .frame(width: 200, height: 300)*/
-                            ForEach(1..<5){_ in
-                                HStack(spacing: 4){
-                                    ForEach(0..<1){_ in
-                                        NavigationLink(destination: NotesOnPicture()) {
-                                            Text("Notes")
-                                                .frame(width: 390,height: 600)
-                                                .background(Color.green)
-                                                .foregroundColor(.white)
-                                        }
+                ScrollView{
+                        ForEach(1..<5){_ in
+                            HStack(spacing: 4){
+                                ForEach(0..<1){_ in
+                                    if(notesShown == true) {
+                                        Text("")
+                                            .frame(width: 390,height: 600)
+                                            .background(Color.green)
+                                            .foregroundColor(.white)
+                                            .overlay(notesOnPicture, alignment: .center)
+                                            .onTapGesture(count: 1) {
+                                                notesShown.toggle()
+                                            }
+                                    } else {
+                                        Text("")
+                                            .frame(width: 390,height: 600)
+                                            .background(Color.green)
+                                            .foregroundColor(.white)
+                                            .onTapGesture(count: 1) {
+                                                notesShown.toggle()
+                                            }
                                     }
                                 }
                             }
                         }
-                        Spacer()
-                    }
+
                 }
-            }.navigationBarTitle("Country", displayMode: .inline)
+            }.navigationBarTitle("Trip place", displayMode: .inline)
+        }
+    }
+    
+    private var notesOnPicture: some View {
+        VStack{
+            HStack{
+                VStack{
+                    //PROFILEPIC JA DATE ADDED
+                    Text("\(Date(), style: .date)")
+                }
+                VStack{
+                    //IF SHARED
+                    Text("Shared")
+                }
+                .padding()
+                VStack{
+                    //TIMEADDED
+                    Text("\(Date(), style: .time)")
+                }
+            }
+            Spacer()
+            HStack {
+                Image(systemName:"person.fill")
+                    .font(.system(size: 50))
+                Circle()
+                    .fill(Color.red)
+                    .frame(width: 150, height: 150)
+                    .padding(30)
+            }
+            Spacer()
+            HStack {
+                //NOTES
+                Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+            } .frame(width: 350)
+            Spacer()
         }
     }
 }
+
+
+
+
 
 struct PicturesInTripsView_Previews: PreviewProvider {
     static var previews: some View {
